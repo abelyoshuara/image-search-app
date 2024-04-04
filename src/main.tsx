5a9547743951264912ca31614bc428bc7cba4253
Abel Yoshuara <abelyoshuara@gmail.com>
2024-04-04 13:12:14 +0800

    setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "@radix-ui/themes/styles.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Theme } from "@radix-ui/themes";
+import routes from "./routes.tsx";
+import "@radix-ui/themes/styles.css";
 import "./index.css";
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Theme>
-      <App />
+      <RouterProvider router={router} />
     </Theme>
   </React.StrictMode>,
 );
